d05e4723a2212e264f5285b3a1ed6728b81f19f6
Miguel R <d4t4s3c@protonmail.com>
2022-05-16 14:13:05 +0200

    UPDATE

--- CRTEnum.sh
+++ CRTEnum.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
 
-declare -r White="\e[97m"
-declare -r Red="\e[31m"
-declare -r Green="\e[32m"
-declare -r YellowLight="\e[93m"
-declare -r Cyan="\e[36m"
-declare -r CyanLight="\e[96m"
-declare -r End="\e[0m"
+declare -r WHITE="\e[97m"
+declare -r RED="\e[31m"
+declare -r GREEN="\e[32m"
+declare -r YELLOW="\e[93m"
+declare -r CYAN="\e[36m"
+declare -r CYANLIGHT="\e[96m"
+declare -r END="\e[0m"
 
-declare -r var1='['
-declare -r var2=']'
-declare -r var3='i'
-declare -r var4='<'
-declare -r var5='>'
-declare -r var6='+'
-declare -r var7='-'
-declare -r var8='x'
-declare -r var9='ERROR!'
-declare -r var10='Target:'
-declare -r var11='Example:'
-declare -r var12='domain.com'
-declare -r var13='OK'
-declare -r var14='NO exist subdomains'
-declare -r var15='Dump Subdomains'
-declare -r var16='Enum Subdomains'
-declare -r var17='CRTEnum'
-declare -r var18='============================================='
+declare -r VAR1='['
+declare -r VAR2=']'
+declare -r VAR3='i'
+declare -r VAR4='<'
+declare -r VAR5='>'
+declare -r VAR6='+'
+declare -r VAR7='-'
+declare -r VAR8='x'
+declare -r VAR9='ERROR!'
+declare -r VAR10='Target:'
+declare -r VAR11='Example:'
+declare -r VAR12='domain.com'
+declare -r VAR13='OK'
+declare -r VAR14='NO exist subdomains'
+declare -r VAR15='Dump Subdomains'
+declare -r VAR16='Enum Subdomains'
+declare -r VAR17='CRTEnum'
+declare -r VAR18='-============================================-'
 
 function banner(){
-    echo -e "$YellowLight"
+    echo -e "$YELLOW"
     echo -e "   __________  ____________                     "
     echo -e "  / ____/ __ \/_  __/ ____/___  __  ______ ___  "
     echo -e " / /   / /_/ / / / / __/ / __ \/ / / / __ \`__ \\"
     echo -e "/ /___/ _, _/ / / / /___/ / / / /_/ / / / / / / "
     echo -e "\____/_/ |_| /_/ /_____/_/ /_/\__,_/_/ /_/ /_/ "
-    echo -e "$White$var18$End"
+    echo -e "$WHITE$VAR18$END"
 } 
 
 function help(){
     echo ""
-    echo -e "$White$var1$YellowLight$var3$White$var2 $Green$var11 $White$var17 -d $Red$var4$White$var12$Red$var5$End"
+    echo -e "$WHITE$VAR1$YELLOW$VAR3$WHITE$VAR2 $GREEN$VAR11 $WHITE$VAR17 -d $RED$VAR4$WHITE$VAR12$RED$VAR5$END"
     echo ""
 }
 
 function status(){
-    echo -e "$White$var1$Red$var7$White$var2 $White$var10 $Green$domain$End"
+    echo -e "$WHITE$VAR1$RED$VAR7$WHITE$VAR2 $WHITE$VAR10 $GREEN$DOMAIN$END"
     sleep 2
-    echo -e "$White$var1$YellowLight$var3$White$var2 $White$var16 $White$var1$Green$var13$White$var2$End"
+    echo -e "$WHITE$VAR1$YELLOW$VAR3$WHITE$VAR2 $WHITE$VAR16 $WHITE$VAR1$GREEN$VAR13$WHITE$VAR2$END"
     sleep 2
 }
 
 function check(){
-        ch=$(curl -s -X GET "https://crt.sh/?q=$domain" | html2text | grep "Certificates" | awk '{print $2}')
+        ch=$(curl -s -X GET "https://crt.sh/?q=$DOMAIN" | html2text | grep "Certificates" | awk '{print $2}')
     if [ "$(echo $ch)" == "None" ]; then
-        echo -e "$White$var1$Red$var8$White$var2 $Red$var9 $White$var14$End"
+        echo -e "$WHITE$VAR1$RED$VAR8$WHITE$VAR2 $RED$VAR9 $WHITE$VAR14$END"
         echo ""
         sleep 2
         exit 1
@@ -63,14 +63,14 @@ function check(){
 }
 
 function enum(){
-    echo -e "$White$var1$Green$var6$White$var2 $White$var15 $White$var1$Green$var13$White$var2$End"
+    echo -e "$WHITE$VAR1$GREEN$VAR6$WHITE$VAR2 $WHITE$VAR15 $WHITE$VAR1$GREEN$VAR13$WHITE$VAR2$END"
     sleep 2
-    echo -e "$White$var18$End"
-    sc=$(curl -s -X GET "https://crt.sh/?q=$domain" | grep -oP '<TD>.*?</TD>' | grep -v -i -E "style|\*" | sort -u | cut -d\> -f 2 | cut -d\< -f 1)
+    echo -e "$WHITE$VAR18$END"
+    F1=$(curl -s "https://crt.sh/?q=.colddsecurity.com&output=json" | jq -r '.[] | "\(.name_value)\n\(.common_name)"' | sort -u)
     echo ""
-    echo -e "$Cyan$sc$End"
+    echo -e "$GREEN$F1$END"
     echo ""
-    echo -e "$White$var18$End"
+    echo -e "$WHITE$VAR18$END"
     echo ""
 }
 
@@ -82,12 +82,12 @@ function start(){
 
 while getopts ":d:" arg; do
     case $arg in
-        d) domain=$OPTARG;
+        d) DOMAIN=$OPTARG;
         ;;
     esac
 done
 
-if [ ! -z $domain ]; then
+if [ ! -z $DOMAIN ]; then
 	:
 else
 	banner
